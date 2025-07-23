d6814b352fe975ad0b7fcb0507df0c01d77dfacf
Max <maxtheman2105@gmail.com>
2025-07-22 22:19:24 -0500

    Some bugs found, if taskbar is open it doesn't work properly, gotta figure out how to remove taskbar

--- Main/Detection.py
+++ Main/Detection.py
@@ -2,24 +2,32 @@ import cv2
 import numpy as np
 import os
 
-large_image = cv2.imread('Data/small.png')
+large_image = cv2.imread('Data/test1.png')
 template_image = cv2.imread('Data/burger.png')
 height, width = large_image.shape[:2]
 threshold = 0.8
 
 large_gray = cv2.cvtColor(large_image, cv2.COLOR_BGR2GRAY)
 template_gray = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
-result = cv2.matchTemplate(large_gray, template_gray, cv2.TM_CCOEFF_NORMED)
+loc = (np.array([], dtype=np.int64), np.array([], dtype=np.int64))
+i = 1
+while (loc[0].size == 0 and loc[1].size == 0) and i < 10:
+    result = cv2.matchTemplate(large_gray, template_gray, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(result >= threshold)
+    height, width = large_gray.shape[:2]
+    print(loc)
+    height += height // 10
+    width += width // 10
+    large_gray = cv2.resize(large_gray, (height, width))
+    i += 1
 
-loc = np.where(result >= threshold)
 start_h = loc[0][0]
 start_w = loc[1][0]
 
 cropped_image = large_image[start_h: start_h + height, start_w: start_w + width]
 cropped_image = cv2.resize(cropped_image, (1344, 756))
-
-
-
+cv2.imshow("Cropped Image", cropped_image)
+cv2.waitKey(0)
 
 pick_w, pick_h = 535, 300 # resize the pick image to this size and then
 crop_w, crop_h = 220, 105 # crop the *center* to this size
@@ -51,8 +59,6 @@ def load_bans(folder="centered/"):
         start_y = (pick_h - crop_h - 50) // 2
         icon = icon[start_y:start_y + crop_h, start_x:start_x + crop_w]        
         templates[champ] = icon
-        # cv2.imshow("was", icon)
-        # cv2.waitKey(0)
     return templates
 
 template = load_bans()
@@ -68,7 +74,6 @@ def match_icon(crop, templates):
             best_champ = champ
     return best_champ, best_score
 
-
 picks = [52, 165, 277, 423, 535] # 232 & 1326
 bans = [27, 117, 207, 347, 437, 844, 934, 1074, 1164, 1254] # 650
 
@@ -77,6 +82,8 @@ for i in picks:
     crop = cropped_image[i: i+107, 9: 9+223] # 232
     crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     champ, score = match_icon(crop, templates)
+    cv2.imshow("Blue Pick: " + champ, crop)
+    cv2.waitKey(0)
     print(champ)
 
 print("RED PICKS:")
@@ -84,6 +91,8 @@ for i in picks:
     crop = cropped_image[i: i+107, 1103: 1103+223]  # 1326
     crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     champ, score = match_icon(crop, templates)
+    cv2.imshow("Blue Pick: " + champ, crop)
+    cv2.waitKey(0)
     print(champ)
 
 print ("BANS:")
@@ -91,9 +100,6 @@ for i in bans:
     crop = cropped_image[650: 650 + 85, i: i + 53]
     crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     champ, score = match_icon(crop, template)    
+    cv2.imshow("Blue Pick: " + champ, crop)
+    cv2.waitKey(0)
     print(champ)
-
-cv2.imshow('Detected Matches', cropped_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
